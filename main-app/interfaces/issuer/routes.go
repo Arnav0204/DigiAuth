fa1c00d11bd4e34421dcb26d3fba079331315b4a
Arnav0204 <anjoglekar0204@gmail.com>
2024-09-27 11:42:15 +0530

    Change req method

--- main-app/interfaces/issuer/routes.go
+++ main-app/interfaces/issuer/routes.go
@@ -11,7 +11,7 @@ func RegisterRoutes() *mux.Router {
 	r.HandleFunc("/register-did", RegisterDID).Methods("POST")
 	r.HandleFunc("/send-invitation", CreateInvitation).Methods("POST")
 	r.HandleFunc("/receive-invitation", ReceiveInvitation).Methods("POST")
-	r.HandleFunc("/connections", GetConnections).Methods("GET")
+	r.HandleFunc("/connections", GetConnections).Methods("POST")
 	r.HandleFunc("/issue-credential", IssueCredential).Methods("POST")
 	return r
 }
