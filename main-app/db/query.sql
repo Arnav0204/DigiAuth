37d99027d1fd3fac93538663eb2edd869eb55708
Arnav0204 <anjoglekar0204@gmail.com>
2024-10-01 21:11:36 +0530

    created table for schemas

--- main-app/db/query.sql
+++ main-app/db/query.sql
@@ -6,3 +6,11 @@ WHERE id = $1;
 -- name: CreateConnection :exec
 INSERT INTO connections (connection_id, id, alias, my_role)
 VALUES ($1, $2, $3, $4);
+
+--name: CreateSchema :exec
+INSERT INTO schemas (schema_id,credential_definition_id,schema_name)
+VALUES ($1, $2, $3);
+
+--name: GetSchema :many
+SELECT *
+FROM schemas;
